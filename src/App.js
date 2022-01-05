53851ba06cede66625694c32becd61246267dea7
Independent <38752974+surachokh@users.noreply.github.com>
2022-01-05 15:31:00 +0700

    Add: Feedback Component
    Adding Feedback Component and mocking data for Feedback Component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className="container">
-        <h1 style={{textAlign: 'center'}}>My App</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from "./FeedbackItem";
+
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback !!!</p>;
+  }
+
+  return (
+    <div>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedbackList;
