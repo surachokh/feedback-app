b0636b69767e334924bd5d73a8aec42aba3a47fa
Independent <38752974+surachokh@users.noreply.github.com>
2022-01-05 15:59:42 +0700

    Add: Feedback Stats Component
    Adding Feedback Stats component to calculate avg. of review point.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
